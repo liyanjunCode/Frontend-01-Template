8f9c826b691af6592d70e22ff3710bcac1fc5e02
liyanjun <254948133@qq.com>
2020-04-23 16:56:18 +0800

    utf8编码修改

--- week02/index.js
+++ week02/index.js
@@ -5,31 +5,42 @@
     let reg =/[+|-]?([0-9]*)\.?([0-9]*)/
 
 
-    // utf8编码
-    // function UTF8Encode(str) {
-    //     let encodeStr = '';
-    //     for(let item of str) {
-    //         const point = item.codePointAt(0)
-    //         let hexadecimal = '\\u' + point.toString(16).padStart(4, '0');
-    //         const binary = item.codePointAt(0).toString(2);
-    //         const len = binary.length
-    //         if(hexadecimal >= '\u0000' && hexadecimal <= '\u007F') {
-    //             console.log(1111, hexadecimal)
-    //             encodeStr += binary.padStart(8, '0')  
-    //         } else if (hexadecimal >= '\u0080' && hexadecimal <= '\u07FF') {
-    //             console.log(22222)
-    //             encodeStr += 110 + binary.substr(0, len).padStart(5, '0') + 10 + binary.substr(len - 1).padStart(6, '0');
-    //         } else if (hexadecimal >= '\u0800' && hexadecimal <= '\uFFFF') {
-    //             console.log(3333)
-    //             encodeStr += 1110 + binary.substr(0, len).padStart(4, '0') + 10 + 
-    //                         binary.substr(len - 11).padStart(6, '0') + 10 + binary.substr(len - 5).padStart(6, '0')
-    //         } else {
-    //             console.log(hexadecimal, 444)
-    //             encodeStr += 11110 + binary.substr(0, len).padStart(3, '0') + 10 + 
-    //                     binary.substr(len - 17).padStart(6, '0') + 10 + binary.substr(len - 5).padStart(6, '0') + 
-    //                     binary.substr(len - 5).padStart(6, '0')
-    //         }
-    //     }
-    //     return encodeStr
-    // }
-    // console.log(UTF8Encode('严'))
+    function UTF8Encode(str) {
+        let encodeStr = '';
+        const dealSub = function(str, byteLen) {
+            const len = str.length;
+            let strLen = byteLen;
+            let strArr = [];
+            while(strLen-- > 0) {
+                if(strLen) {
+                    strArr.push(`10${str.substr(len - 6 * (3 - strLen), 6).padStart(6, '0')}`)
+                } else {
+                    let str1 = '';
+                    for(let i=0; i<byteLen; i++) {
+                        str1 += 1;
+                    }
+                    str1 +=0;
+                    strArr.push(`${str1}${str.substring(0, len - 6 * (2 - strLen)).padStart(7 - byteLen, '0')}`)
+                }
+            }
+            return strArr.reverse().join(' ')
+        }
+        for(let item of str) {
+            const point = item.codePointAt(0)
+            const hexadecimal = '0x' +point.toString(16)
+            const binary = point.toString(2);
+            const len = binary.length;
+            if (hexadecimal >= 0x0000 && hexadecimal <= 0x007F){
+                encodeStr += binary.padStart(8, '0')
+            }else if (hexadecimal >= 0x0080 && hexadecimal <= 0x07FF) {
+                encodeStr += dealSub(binary, 2) + ' '
+            } else if (hexadecimal >= 0x0800 && hexadecimal <= 0xFFFF) {
+                encodeStr += dealSub(binary, 3) + ' '
+            } else if (hexadecimal <= 0x10FFFF) {
+                encodeStr += dealSub(binary, 4) + ' '
+            }
+        }
+        return encodeStr
+    }
+    // console.log(UTF8Encode('a'))
+    console.log(UTF8Encode('严'))
